6d4e37c01f3a6a7b4ac72fc78d2be37b3b44f73a
Thalisson <thalissonpop91@gmail.com>
2024-02-18 22:21:39 -0300

    feat: initial logic to open chat

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ArrowRightComponent } from '../../icons/arrow-right/arrow-right.component';
 import { SparkleComponent } from '../../icons/sparkle/sparkle.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,9 @@ import { SparkleComponent } from '../../icons/sparkle/sparkle.component';
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {}
+
+  openChat() {
+    this.router.navigate(["chat"])
+  }
 }
